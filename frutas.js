93393168072231da237f82e490ca28b1eb57e17c
vanessamendoza <vanessam363@gmail.com>
2016-08-18 15:34:02 -0400

    Create frutas.js
    linked javascript to frutas.html

---
+++ frutas.js
@@ -0,0 +1,27 @@
+function apples() {
+		document.getElementById("apples").innerHTML = "Apples";
+	}
+function pears() {
+	document.getElementById("pears").innerHTML = "Pear";
+    }
+function cherry() {
+	document.getElementById("cherry").innerHTML = "Cherry";
+}
+function strawberry() {
+	document.getElementById("strawberry").innerHTML = "Strawberry";
+}
+function mango() {
+	document.getElementById("mango").innerHTML = "Mango";
+}
+function grapefruit() {
+	document.getElementById("grapefruit").innerHTML = "Grapefruit";
+}
+function pomegranate() {
+	document.getElementById("pomegranate").innerHTML = "Pomegranate";
+}
+function watermelon() {
+	document.getElementById("watermelon").innerHTML = "Watermelon";
+}
+function avocado(){
+    document.getElementById("avocado").innerHTML = "Avocado";
+}
