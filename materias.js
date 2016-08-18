09dce8a2a7e4ee2cdeae81c7224886414e0f3247
anasantos99 <ana.santos.0818@gmail.com>
2016-08-18 13:46:24 -0400

    javascript for buttons for materias window

---
+++ materias.js
@@ -0,0 +1,24 @@
+function writing() {
+		document.getElementById("writing").innerHTML = "Writing";
+	}
+function science() {
+	document.getElementById("science").innerHTML = "Science";
+}
+function history() {
+	document.getElementById("history").innerHTML = "History";
+}
+function gym() {
+	document.getElementById("gym").innerHTML = "Physical Education";
+}
+function chem() {
+	document.getElementById("chem").innerHTML = "Chemistry";
+}
+function art() {
+	document.getElementById("art").innerHTML = "Art";
+}
+function math() {
+	document.getElementById("math").innerHTML = "Math";
+}
+function music() {
+	document.getElementById("music").innerHTML = "Music";
+}
