d9631a8a62b691e090a0e66affc5dd48b837ee78
jami22 <jamiles22@me.com>
2016-08-18 15:12:23 -0400

    added java to button

---
+++ physicalterms.js
@@ -0,0 +1,24 @@
+function bloodpressure() {
+		document.getElementById("bloodpressure").innerHTML = "Blood Pressure";
+	}
+function heartrate() {
+	document.getElementById("heartrate").innerHTML = "Heart Rate";
+}
+function respirationrate() {
+	document.getElementById("respirationrate").innerHTML = "Respiration Rate";
+}
+function temperature() {
+	document.getElementById("temperature").innerHTML = "Temperature";
+}
+function chem() {
+	document.getElementById("chem").innerHTML = "Chemistry";
+}
+function art() {
+	document.getElementById("art").innerHTML = "Art";
+}
+function math() {
+	document.getElementById("math").innerHTML = "Math";
+}
+function music() {
+	document.getElementById("music").innerHTML = "Music";
+}
