d59fca56878fc648520ae1084ae6c073143a9a4b
jami22 <jamiles22@me.com>
2016-08-19 15:24:29 -0400

    created translations

---
+++ medical.js
@@ -0,0 +1,48 @@
+function allergy() {
+	document.getElementById("allergy").innerHTML = "Allergy";
+}
+function alzheimersdisease() {
+	document.getElementById("alzheimersdisease").innerHTML = "Alzheimer's Disease";
+}
+function arthritis() {
+	document.getElementById("arthritis").innerHTML = "Arthitis";
+}
+function asthma() {
+	document.getElementById("asthma").innerHTML = "Asthma";
+}
+function cholesterol() {
+	document.getElementById("cholesterol").innerHTML = "Cholesterol";
+}
+function chronicpain() {
+	document.getElementById("chronicpain").innerHTML = "Chronic Pain";
+}
+function flu() {
+	document.getElementById("flu").innerHTML = "Flu";
+}
+function depression() {
+	document.getElementById("depression").innerHTML = "Depression";
+}
+function abdominalpain() {
+	document.getElementById("abdominalpain").innerHTML = "Abdominal Pain";
+}
+function cataracts() {
+	document.getElementById("cataracts").innerHTML = "Cataracts";
+}
+function heartattack() {
+	document.getElementById("heartattack").innerHTML = "Heart Attack";
+}
+function highbloodpressure() {
+		document.getElementById("highbloodpressure").innerHTML = "High Blood Pressure";
+	}
+function menopause() {
+	document.getElementById("menopause").innerHTML = "Menopause";
+}
+function anxiety() {
+	document.getElementById("anxiety").innerHTML = "Anxiety";
+}
+function migraine() {
+	document.getElementById("migraine").innerHTML = "Migraine";
+}
+function insomnia() {
+	document.getElementById("insomnia").innerHTML = "Insomnia";
+}
