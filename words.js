6cfa0e45c7272b8281fd4b188334617a52b5e588
Cathyln15 <sthmendoza.cat@gmail.com>
2016-08-19 15:48:44 -0400

    created translation

---
+++ words.js
@@ -0,0 +1,113 @@
+function si() {
+		document.getElementById("si").innerHTML = "Yes";
+	}
+function no() {
+	document.getElementById("no").innerHTML = "No";
+}
+function mon() {
+	document.getElementById("mon").innerHTML = "Monday";
+}
+function tues() {
+	document.getElementById("tues").innerHTML = "Tuesday";
+}
+function mi() {
+	document.getElementById("mi").innerHTML = "Wendnesday";
+}
+function hi() {
+	document.getElementById("hi").innerHTML = "Hello";
+}
+function what() {
+	document.getElementById("what").innerHTML = "What?";
+}
+function ju() {
+	document.getElementById("ju").innerHTML = "Thursday";
+}
+function frid() {
+	document.getElementById("frid").innerHTML = "Friday";
+}
+function saba() {
+	document.getElementById("saba").innerHTML = "Saturday";
+}
+function domi() {
+	document.getElementById("domi").innerHTML = "Sunday";
+}
+function office() {
+	document.getElementById("office").innerHTML = "Office";
+}
+function cost() {
+	document.getElementById("cost").innerHTML = "Estimate";
+}
+function deposit() {
+	document.getElementById("deposit").innerHTML = "To deposit";
+}
+function salir() {
+	document.getElementById("salir").innerHTML = "   Exit";
+}
+function pencil() {
+	document.getElementById("pencil").innerHTML = "Pencil";
+	
+}
+function pen() {
+	document.getElementById("pen").innerHTML = "Pen";
+	
+}
+function papel() {
+	document.getElementById("papel").innerHTML = "Paper";
+	
+}
+function table() {
+	document.getElementById("table").innerHTML = "Table";
+	
+}
+function way() {
+	document.getElementById("way").innerHTML = "Hallway";
+	
+}
+function stairs() {
+	document.getElementById("stairs").innerHTML = "Stairs";
+	
+}
+function room() {
+	document.getElementById("room").innerHTML = "Bathroom";
+	
+}
+function roof() {
+	document.getElementById("roof").innerHTML = "Roof";
+	
+}
+function garg() {
+	document.getElementById("garg").innerHTML = "Garbage";
+	
+}
+function weather() {
+	document.getElementById("weather").innerHTML = "Weather";
+	
+}
+function room() {
+	document.getElementById("room").innerHTML = "Bathroom";
+	
+}
+function door() {
+	document.getElementById("door").innerHTML = "Door";
+	
+}
+function restu() {
+	document.getElementById("restu").innerHTML = "Resturant";
+	
+}
+function store() {
+	document.getElementById("store").innerHTML = "Store";
+	
+}
+function food() {
+	document.getElementById("food").innerHTML = "Food";
+	
+}
+function suit() {
+	document.getElementById("suit").innerHTML = "Suit";
+	
+}
+function chair() {
+	document.getElementById("chair").innerHTML = "Chair";
+	
+}
