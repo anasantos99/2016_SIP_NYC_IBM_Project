c27a8d0f14f480706a5996dd173dd5f962563486
jami22 <jamiles22@me.com>
2016-08-19 15:23:46 -0400

    created translations

---
+++ questions.js
@@ -0,0 +1,24 @@
+function why() {
+		document.getElementById("why").innerHTML = "Why are you here today?";
+	}
+function how() {
+	document.getElementById("how").innerHTML = "How are you feeling?";
+}
+function have() {
+	document.getElementById("have").innerHTML = "Have you had any pain lately?";
+}
+function when() {
+	document.getElementById("when").innerHTML = "When did this start?";
+}
+function at() {
+	document.getElementById("at").innerHTML = "What time do you go to sleep?";
+}
+function what() {
+	document.getElementById("what").innerHTML = "What do you do on a daily basis?";
+}
+function diet() {
+	document.getElementById("diet").innerHTML = "What does your diet look like?";
+}
+function exercise() {
+	document.getElementById("exercise").innerHTML = "What is your exercise routine?";
+}
