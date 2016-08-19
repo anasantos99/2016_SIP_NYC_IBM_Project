979330c8229e5319d13977ebdd40863053d6e7d8
Cathyln15 <sthmendoza.cat@gmail.com>
2016-08-19 15:49:48 -0400

    created translation

---
+++ communication.js
@@ -0,0 +1,33 @@
+function dias() {
+		document.getElementById("dias").innerHTML = "GoodMorning";
+	}
+function dia() {
+	document.getElementById("dia").innerHTML = "Good Afternoon";
+}
+function night() {
+	document.getElementById("night").innerHTML = "Good Night";
+}
+function gusto() {
+	document.getElementById("gusto").innerHTML = "Nice to meet you";
+}
+function thnxz() {
+	document.getElementById("thnxz").innerHTML = "Thank You";
+}
+function sorry() {
+	document.getElementById("sorry").innerHTML = "Im sorry";
+}
+function me() {
+	document.getElementById("me").innerHTML = "Excuse Me";
+}
+function please() {
+	document.getElementById("please").innerHTML = "Please";
+}
+function you() {
+	document.getElementById("you").innerHTML = "Bless You";
+}
+function welcome() {
+	document.getElementById("welcome").innerHTML = "You are Welcome";
+}
+function i() {
+	document.getElementById("i").innerHTML = "I do not understand";
+}
