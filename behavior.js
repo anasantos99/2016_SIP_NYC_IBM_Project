34b059069c6674fd7a4307b37355c528c8b9c15c
anasantos99 <ana.santos.0818@gmail.com>
2016-08-19 15:28:09 -0400

    created js file for behavior html file

---
+++ behavior.js
@@ -0,0 +1,36 @@
+function disrespectful() {
+	document.getElementById("disrespectful").innerHTML = "Disrespectful";
+}
+function ignorant() {
+	document.getElementById("ignorant").innerHTML = "Ignorant";
+}
+function attention() {
+	document.getElementById("attention").innerHTML = "Does not pay attention";
+}
+function talk() {
+	document.getElementById("talk").innerHTML = "Talks a lot";
+}
+function quiet() {
+	document.getElementById("quiet").innerHTML = "Quiet";
+}
+function shy() {
+	document.getElementById("shy").innerHTML = "Shy";
+}
+function phone() {
+	document.getElementById("phone").innerHTML = "Always on their phone";
+}
+function participate() {
+	document.getElementById("participate").innerHTML = "Needs to participate more during class";
+}
+function skip() {
+	document.getElementById("skip").innerHTML = "Does not come to class";
+}
+function good() {
+	document.getElementById("good").innerHTML = "Very well behaved";
+}
+function help() {
+	document.getElementById("help").innerHTML = "Helps their classmates";
+}
+function work() {
+	document.getElementById("work").innerHTML = "Very Hardworking";
+}
