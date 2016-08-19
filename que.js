cbc624d08871d083c4a5591b68d302a25755156c
Cathyln15 <sthmendoza.cat@gmail.com>
2016-08-19 15:47:23 -0400

    created translation

---
+++ que.js
@@ -0,0 +1,45 @@
+function printer() {
+		document.getElementById("printer").innerHTML = "Where is the printer?";
+	}
+function jefe() {
+	document.getElementById("jefe").innerHTML = "Where is the boss?";
+}
+function help() {
+	document.getElementById("help").innerHTML = "Can you help me?";
+}
+function think() {
+	document.getElementById("think").innerHTML = "What do you think?";
+}
+function traducir() {
+	document.getElementById("traducir").innerHTML = "May you please translate that?";
+}
+function repeat() {
+	document.getElementById("repeat").innerHTML = "May you please repeat that?";
+}
+function plan() {
+	document.getElementById("plan").innerHTML = "What is the plan?";
+}
+function hora() {
+	document.getElementById("hora").innerHTML = "What time is it?";
+}
+function hoy() {
+	document.getElementById("hoy").innerHTML = "What’s today date?";
+}
+function eso() {
+	document.getElementById("eso").innerHTML = "What is that about?";
+}
+function como() {
+	document.getElementById("como").innerHTML = "How do I do that?";
+}
+function algun() {
+	document.getElementById("algun").innerHTML = "Any Advice?";
+}
+function reunion() {
+	document.getElementById("reunion").innerHTML = "When is the meeting?";
+}
+function debe() {
+	document.getElementById("debe").innerHTML = "When is it due?";
+}
+function normas() {
+	document.getElementById("normas").innerHTML = "What are the rules? ";
+}
