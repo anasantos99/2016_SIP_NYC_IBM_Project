9d454b78504e923a1a20f118c9faaf3e29e86c4e
jami22 <jamiles22@me.com>
2016-08-19 15:22:41 -0400

    created translations

---
+++ bodyparts.js
@@ -0,0 +1,51 @@
+function my___hurtsormy___isbroken() {
+		document.getElementById("my___hurtsormy___isbroken").innerHTML = "My___hurts or my___is broken";
+	}
+function hands() {
+	document.getElementById("hands").innerHTML = "Hands";
+}
+function fingers() {
+	document.getElementById("fingers").innerHTML = "Fingers";
+}
+function arms() {
+	document.getElementById("arms").innerHTML = "Arms";
+}
+function neck() {
+	document.getElementById("neck").innerHTML = "Neck";
+}
+function heads() {
+	document.getElementById("heads").innerHTML = "Head";
+}
+function eyes() {
+	document.getElementById("eyes").innerHTML = "Eyes";
+}
+function nose() {
+	document.getElementById("nose").innerHTML = "Nose";
+}
+function ears() {
+	document.getElementById("ears").innerHTML = "Ears";
+}
+function mouth() {
+	document.getElementById("mouth").innerHTML = "Mouth";
+}
+function legs() {
+	document.getElementById("legs").innerHTML = "Legs";
+}
+function feet() {
+		document.getElementById("feet").innerHTML = "Feet";
+	}
+function ankles() {
+	document.getElementById("ankles").innerHTML = "Ankles";
+}
+function knees() {
+	document.getElementById("knees").innerHTML = "Knees";
+}
+function back() {
+	document.getElementById("back").innerHTML = "Back";
+}
+function stomach() {
+	document.getElementById("stomach").innerHTML = "Stomach";
+}
+function chest() {
+	document.getElementById("chest").innerHTML = "Chest";
+}
