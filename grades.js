238add51dbc4b7953a61481421a69cc4f5fd6c79
anasantos99 <ana.santos.0818@gmail.com>
2016-08-19 15:24:46 -0400

    created js file for grades.html

---
+++ grades.js
@@ -0,0 +1,30 @@
+function pass() {
+	document.getElementById("pass").innerHTML = "Pass the Class";
+}
+function fail() {
+	document.getElementById("fail").innerHTML = "Fail the class";
+}
+function excellent() {
+	document.getElementById("excellent").innerHTML = "Excellente";
+}
+function great() {
+	document.getElementById("great").innerHTML = "Very Good";
+}
+function poor() {
+	document.getElementById("poor").innerHTML = "Poor";
+}
+function average() {
+	document.getElementById("average").innerHTML = "Average";
+}
+function improve() {
+	document.getElementById("improve").innerHTML = "Needs Improvement";
+}
+function progress() {
+	document.getElementById("progress").innerHTML = "Good Progress";
+}
+function danger() {
+	document.getElementById("danger").innerHTML = "Indanger of failing the class";
+}
+function advanced() {
+	document.getElementById("advanced").innerHTML = "Is ahead of the rest of the class";
+}
