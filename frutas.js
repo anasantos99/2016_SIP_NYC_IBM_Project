05065d8a8d872ba35efd1e7eec37837f0d6a4a4d
vanessamendoza <vanessam363@gmail.com>
2016-08-20 19:44:35 -0400

    Update frutas.js
    added an id for the vegetables and fruits.

--- frutas.js
+++ frutas.js
@@ -1,9 +1,9 @@
 function apples() {
 		document.getElementById("apples").innerHTML = "Apples";
-	}
+}
 function pears() {
 	document.getElementById("pears").innerHTML = "Pear";
-    }
+}
 function cherry() {
 	document.getElementById("cherry").innerHTML = "Cherry";
 }
@@ -19,9 +19,52 @@ function grapefruit() {
 function pomegranate() {
 	document.getElementById("pomegranate").innerHTML = "Pomegranate";
 }
-function watermelon() {
-	document.getElementById("watermelon").innerHTML = "Watermelon";
+function pineapple() {
+	document.getElementById("pineapple").innerHTML = "Pineapple";
 }
-function avocado(){
-    document.getElementById("avocado").innerHTML = "Avocado";
+function papaya(){
+	document.getElementById("papaya").innerHTML = "Papaya";
 }
+
+function orange() { 
+ 	document.getElementById("orange").innerHTML = "Orange"; 
+} 
+function tomato() { 
+ 	document.getElementById("tomato").innerHTML = "Tomato"; 
+} 
+function plum() { 
+	document.getElementById("plum").innerHTML = "Plum"; 
+} 
+function raspberry() { 
+ 	document.getElementById("raspberry").innerHTML = "Raspberry"; 
+} 
+function banana() { 
+ 	document.getElementById("banana").innerHTML = "Banana"; 
+} 
+function blueberry() { 
+ 	document.getElementById("blueberry").innerHTML = "Bluberry"; 
+} 
+function broccoli() { 
+	document.getElementById("broccoli").innerHTML = "Broccoli"; 
+} 
+function lettuce() { 
+	document.getElementById("lettuce").innerHTML = "Lettuce"; 
+} 
+function cucumbers() { 
+ 	document.getElementById("cucumbers").innerHTML = "Cucumbers"; 
+} 
+function carrot() { 
+	document.getElementById("carrot").innerHTML = "Carrot"; 
+} 
+function pepper() { 
+ 	document.getElementById("pepper").innerHTML = "Pepper"; 
+} 
+function celery() { 
+ 	document.getElementById("celery").innerHTML = "Celery"; 
+} 
+function spinach() { 
+	document.getElementById("spinach").innerHTML = "spinach"; 
+} 
+function potatoes() { 
+	document.getElementById("potatoes").innerHTML = "Potatoes"; 
+} 
