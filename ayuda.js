f067d235da71713e3f70af5ede5cc9ce9d2d3152
vanessamendoza <vanessam363@gmail.com>
2016-08-20 20:39:10 -0400

    Create ayuda.js
    added js file for ayuda.html

---
+++ ayuda.js
@@ -0,0 +1,13 @@
+
+function where is....?() {
+	document.getElementById("where is....?").innerHTML = "Where Is....?";
+}
+function where are the....?() {
+	document.getElementById("where are the....?").innerHTML = "Where Are The....?";
+}
+function how much?() {
+	document.getElementById("how much?").innerHTML = "How Much?";
+}
+function where can I pay?() {
+	document.getElementById("where can I pay?").innerHTML = "Where Can I Pay?";
+}
