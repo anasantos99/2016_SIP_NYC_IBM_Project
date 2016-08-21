ba82385860870a59939b266c8c583a0e2aa19270
vanessamendoza <vanessam363@gmail.com>
2016-08-20 21:05:24 -0400

    Create productos.js
    added a js file for productos.html

---
+++ productos.js
@@ -0,0 +1,57 @@
+function milk() {
+		document.getElementById("milk").innerHTML = "Milk";
+	}
+function cheese() {
+	document.getElementById("cheese").innerHTML = "Cheese";
+}
+function soap() {
+	document.getElementById("soap").innerHTML = "Soap";
+}
+function salt() {
+	document.getElementById("salt").innerHTML = "Salt";
+}
+function meat() {
+	document.getElementById("meat").innerHTML = "Meat";
+}
+function chicken() {
+	document.getElementById("chicken").innerHTML = "Chicken";
+}
+function yogurt() {
+	document.getElementById("yogurt").innerHTML = "Yogurt";
+}
+function bread() {
+	document.getElementById("bread").innerHTML = "Bread";
+}
+function coffee() {
+	document.getElementById("coffee").innerHTML = "Coffee";
+}
+function juice() {
+	document.getElementById("juice").innerHTML = "Juice";
+}
+function water() {
+	document.getElementById("water").innerHTML = "Water";
+}
+function tea() {
+	document.getElementById("tea").innerHTML = "Tea";
+}
+function sugar() {
+	document.getElementById("sugar").innerHTML = "Sugar";
+}
+function rice() {
+	document.getElementById("rice").innerHTML = "Rice";
+}
+function oil() {
+	document.getElementById("oil").innerHTML = "Oil";
+}
+function oatmeal() {
+	document.getElementById("oatmeal").innerHTML = "Oatmeal";
+}
+function napkins() {
+	document.getElementById("napkins").innerHTML = "Napkins";
+}
+function eggs() {
+	document.getElementById("eggs").innerHTML = "Eggs";
+}
+function fish() {
+	document.getElementById("fish").innerHTML = "Fish";
+}
