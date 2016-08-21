dcccf603c24f9bf5cb53a6fd4bd36472d2dc437d
vanessamendoza <vanessam363@gmail.com>
2016-08-20 20:10:57 -0400

    Create numeros.js
    added a js file for numeros.html

---
+++ numeros.js
@@ -0,0 +1,33 @@
+function one() {
+		document.getElementById("one").innerHTML = "One";
+	}
+function two() {
+	document.getElementById("two").innerHTML = "Two";
+}
+function three() {
+	document.getElementById("three").innerHTML = "Three";
+}
+function four() {
+	document.getElementById("four").innerHTML = "Four";
+}
+function five() {
+	document.getElementById("five").innerHTML = "Five";
+}
+function six() {
+	document.getElementById("six").innerHTML = "Six";
+}
+function seven() {
+	document.getElementById("seven").innerHTML = "Seven";
+}
+function eight() {
+	document.getElementById("eight").innerHTML = "Eight";
+}
+function nine() {
+	document.getElementById("nine").innerHTML = "Nine";
+}
+function ten() {
+	document.getElementById("ten").innerHTML = "Ten";
+}
+function ninety-nine() {
+	document.getElementById("ninety-nine").innerHTML = "Ninety-Nine";
+}
