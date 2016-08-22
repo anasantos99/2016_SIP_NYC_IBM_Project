e5733846c507abae7b72905308d8fe0c4a6a4245
anasantos99 <ana.santos.0818@gmail.com>
2016-08-22 14:06:01 -0400

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,36 +1,36 @@
 function disrespectful() {
-	document.getElementById("disrespectful").innerHTML = "Disrespectful";
+	document.getElementById("disrespectful").innerHTML = "Disrespectful (disrispectful)";
 }
 function ignorant() {
-	document.getElementById("ignorant").innerHTML = "Ignorant";
+	document.getElementById("ignorant").innerHTML = "Ignorant (ignorent)";
 }
 function attention() {
-	document.getElementById("attention").innerHTML = "Does not pay attention";
+	document.getElementById("attention").innerHTML = "Does not pay attention (dos nat pei atenchon)";
 }
 function talk() {
-	document.getElementById("talk").innerHTML = "Talks a lot";
+	document.getElementById("talk").innerHTML = "Talks a lot (talks a lot)";
 }
 function quiet() {
-	document.getElementById("quiet").innerHTML = "Quiet";
+	document.getElementById("quiet").innerHTML = "Quiet (cuaiet)";
 }
 function shy() {
-	document.getElementById("shy").innerHTML = "Shy";
+	document.getElementById("shy").innerHTML = "Shy (shai)";
 }
 function phone() {
-	document.getElementById("phone").innerHTML = "Always on their phone";
+	document.getElementById("phone").innerHTML = "Always on their phone (oluais on deur fon)";
 }
 function participate() {
-	document.getElementById("participate").innerHTML = "Needs to participate more during class";
+	document.getElementById("participate").innerHTML = "Needs to participate more during class (nids tu partisipait mor durin clas)";
 }
 function skip() {
-	document.getElementById("skip").innerHTML = "Does not come to class";
+	document.getElementById("skip").innerHTML = "Does not come to class (doe nat com tu clas)";
 }
 function good() {
-	document.getElementById("good").innerHTML = "Very well behaved";
+	document.getElementById("good").innerHTML = "Very well behaved (veri uel bijaivd)";
 }
 function help() {
-	document.getElementById("help").innerHTML = "Helps their classmates";
+	document.getElementById("help").innerHTML = "Helps their classmates (jelps deur clasmaits)";
 }
 function work() {
-	document.getElementById("work").innerHTML = "Very Hardworking";
+	document.getElementById("work").innerHTML = "Very Hardworking (veri jard  uorkin)";
 }
