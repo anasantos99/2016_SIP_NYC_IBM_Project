8b79ef794c919b4d41e7edbb23ba2cddbda82d04
jami22 <jamiles22@me.com>
2016-08-22 15:25:06 -0400

    Update

--- physicalterms.js
+++ physicalterms.js
@@ -1,33 +1,77 @@
 function bloodpressure() {
-		document.getElementById("bloodpressure").innerHTML = "Blood Pressure(blod pressur)";
+		if(document.getElementById("bloodpressure").innerHTML == "Blood Pressure(blod pressur)"){
+           document.getElementById("bloodpressure").innerHTML = "";  
+        } else {
+        document.getElementById("bloodpressure").innerHTML = "Blood Pressure(blod pressur)";
+        }
 	}
 function heartrate() {
-	document.getElementById("heartrate").innerHTML = "Heart Rate(hart rat)";
+	if(document.getElementById("heartrate").innerHTML == "Heart Rate(hart rat)"){
+        document.getElementById("heartrate").innerHTML = "";
+    } else {
+        document.getElementById("heartrate").innerHTML = "Heart Rate(hart rat)";
+    }
 }
 function respirationrate() {
-	document.getElementById("respirationrate").innerHTML = "Respiration Rate(risperateon rat)";
+	if(document.getElementById("respirationrate").innerHTML == "Respiration Rate(risperateon rat)"){
+        document.getElementById("respirationrate").innerHTML = "";
+    } else {
+        document.getElementById("respirationrate").innerHTML = "Respiration Rate(risperateon rat)";
+    }
 }
 function temperature() {
-	document.getElementById("temperature").innerHTML = "Temperature(temperature)";
+	if(document.getElementById("temperature").innerHTML == "Temperature(temperature)"){
+        document.getElementById("temperature").innerHTML = "";
+    } else {
+        document.getElementById("temperature").innerHTML = "Temperature(temperature)";
+    }
 }
 function heartexam() {
-	document.getElementById("heartexam").innerHTML = "Heart Exam(hart eksam)";
+	if(document.getElementById("heartexam").innerHTML == "Heart Exam(hart eksam)"){
+        document.getElementById("heartexam").innerHTML = "";
+    } else {
+        document.getElementById("heartexam").innerHTML = "Heart Exam(hart eksam)";
+    }
 }
 function lungexam() {
-	document.getElementById("lungexam").innerHTML = "Lung Exam(lung eksam)";
+	if(document.getElementById("lungexam").innerHTML == "Lung Exam(lung eksam)"){
+        document.getElementById("lungexam").innerHTML = "";
+    } else {
+        document.getElementById("lungexam").innerHTML = "Lung Exam(lung eksam)";
+    }
 }
 function headandneckexam() {
-	document.getElementById("headandneckexam").innerHTML = "Head and Neck Exam(jead and nek eksam)";
+	if(document.getElementById("headandneckexam").innerHTML == "Head and Neck Exam(jead and nek eksam)"){
+        document.getElementById("headandneckexam").innerHTML = "";
+    } else {
+        document.getElementById("headandneckexam").innerHTML = "Head and Neck Exam(jead and nek eksam)";
+    }
 }
 function abdominalexam() {
-	document.getElementById("abdominalexam").innerHTML = "Abdominal Exam(abdominal eksam)";
+	if(document.getElementById("abdominalexam").innerHTML == "Abdominal Exam(abdominal eksam)"){
+        document.getElementById("abdominalexam").innerHTML = "";
+    } else {
+        document.getElementById("abdominalexam").innerHTML = "Abdominal Exam(abdominal eksam)";
+    }
 }
 function neurologicalexam() {
-	document.getElementById("neurologicalexam").innerHTML = "Neurological Exam(nurological eksam)";
+	if(document.getElementById("neurologicalexam").innerHTML == "Neurological Exam(nurological eksam)"){
+        document.getElementById("neurologicalexam").innerHTML = "";
+    } else {
+        document.getElementById("neurologicalexam").innerHTML = "Neurological Exam(nurological eksam)";
+    }
 }
 function dermatologicalexam() {
-	document.getElementById("dermatologicalexam").innerHTML = "Dermatological Exam(dermalogical eksam)";
+	if(document.getElementById("dermatologicalexam").innerHTML == "Dermatological Exam(dermalogical eksam)"){
+        document.getElementById("dermatologicalexam").innerHTML = "";
+    } else {
+        document.getElementById("dermatologicalexam").innerHTML = "Dermatological Exam(dermalogical eksam)";
+    }
 }
 function bloodtest() {
-	document.getElementById("bloodtest").innerHTML = "Blood Test(blod test)";
+	if(document.getElementById("bloodtest").innerHTML == "Blood Test(blod test)"){
+        document.getElementById("bloodtest").innerHTML = "";
+    } else {
+        document.getElementById("bloodtest").innerHTML = "Blood Test(blod test)";
+    }
 }
