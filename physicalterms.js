7f4e320b61592834a36253da07aeefcf1463aaf9
jami22 <jamiles22@me.com>
2016-08-22 13:51:36 -0400

    added pronunciation

--- physicalterms.js
+++ physicalterms.js
@@ -1,33 +1,33 @@
 function bloodpressure() {
-		document.getElementById("bloodpressure").innerHTML = "Blood Pressure";
+		document.getElementById("bloodpressure").innerHTML = "Blood Pressure(blod pressur)";
 	}
 function heartrate() {
-	document.getElementById("heartrate").innerHTML = "Heart Rate";
+	document.getElementById("heartrate").innerHTML = "Heart Rate(hart rat)";
 }
 function respirationrate() {
-	document.getElementById("respirationrate").innerHTML = "Respiration Rate";
+	document.getElementById("respirationrate").innerHTML = "Respiration Rate(risperateon rat)";
 }
 function temperature() {
-	document.getElementById("temperature").innerHTML = "Temperature";
+	document.getElementById("temperature").innerHTML = "Temperature(temperature)";
 }
 function heartexam() {
-	document.getElementById("heartexam").innerHTML = "Heart Exam";
+	document.getElementById("heartexam").innerHTML = "Heart Exam(hart ekam)";
 }
 function lungexam() {
-	document.getElementById("lungexam").innerHTML = "Lung Exam";
+	document.getElementById("lungexam").innerHTML = "Lung Exam(lung ekam)";
 }
 function headandneckexam() {
-	document.getElementById("headandneckexam").innerHTML = "Head and Neck Exam";
+	document.getElementById("headandneckexam").innerHTML = "Head and Neck Exam(jead and nek ekam)";
 }
 function abdominalexam() {
-	document.getElementById("abdominalexam").innerHTML = "Abdominal Exam";
+	document.getElementById("abdominalexam").innerHTML = "Abdominal Exam(abdominal ekam)";
 }
 function neurologicalexam() {
-	document.getElementById("neurologicalexam").innerHTML = "Neurological Exam";
+	document.getElementById("neurologicalexam").innerHTML = "Neurological Exam(nurological ekam)";
 }
 function dermatologicalexam() {
-	document.getElementById("dermatologicalexam").innerHTML = "Dermatological Exam";
+	document.getElementById("dermatologicalexam").innerHTML = "Dermatological Exam(dermalogical ekam)";
 }
 function bloodtest() {
-	document.getElementById("bloodtest").innerHTML = "Blood Test";
+	document.getElementById("bloodtest").innerHTML = "Blood Test(blod test)";
 }
