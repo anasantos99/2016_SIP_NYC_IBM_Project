cf20bcd257399c45f158fbac4702f850b71b1dc7
vanessamendoza <vanessam363@gmail.com>
2016-08-22 13:13:46 -0400

    Update frutas.js
    added pronunciation for the fruits

--- frutas.js
+++ frutas.js
@@ -1,74 +1,73 @@
 function apples() {
-		document.getElementById("apples").innerHTML = "Apples";
+		document.getElementById("apples").innerHTML = "Apples(apels)";
 }
 function pears() {
-	document.getElementById("pears").innerHTML = "Pear";
+	document.getElementById("pears").innerHTML = "Pear(per)";
 }
 function cherry() {
-	document.getElementById("cherry").innerHTML = "Cherry";
+	document.getElementById("cherry").innerHTML = "Cherry(cheri)";
 }
 function strawberry() {
-	document.getElementById("strawberry").innerHTML = "Strawberry";
+	document.getElementById("strawberry").innerHTML = "Strawberry(strabery)";
 }
 function mango() {
-	document.getElementById("mango").innerHTML = "Mango";
+	document.getElementById("mango").innerHTML = "Mango"(mango);
 }
 function grapefruit() {
-	document.getElementById("grapefruit").innerHTML = "Grapefruit";
+	document.getElementById("grapefruit").innerHTML = "Grapefruit(grepfrut)";
 }
 function pomegranate() {
-	document.getElementById("pomegranate").innerHTML = "Pomegranate";
+	document.getElementById("pomegranate").innerHTML = "Pomegranate(pomigranete)";
 }
 function pineapple() {
-	document.getElementById("pineapple").innerHTML = "Pineapple";
+	document.getElementById("pineapple").innerHTML = "Pineapple(pinaple)";
 }
 function papaya(){
-	document.getElementById("papaya").innerHTML = "Papaya";
+	document.getElementById("papaya").innerHTML = "Papaya(papaya)";
 }
 
 function orange() { 
- 	document.getElementById("orange").innerHTML = "Orange"; 
+ 	document.getElementById("orange").innerHTML = "Orange(oranje)"; 
 } 
 function tomato() { 
- 	document.getElementById("tomato").innerHTML = "Tomato"; 
+ 	document.getElementById("tomato").innerHTML = "Tomato(tomayto)"; 
 } 
 function plum() { 
-	document.getElementById("plum").innerHTML = "Plum"; 
+	document.getElementById("plum").innerHTML = "Plum(plom)"; 
 } 
 function raspberry() { 
- 	document.getElementById("raspberry").innerHTML = "Raspberry"; 
+ 	document.getElementById("raspberry").innerHTML = "Raspberry(rasberi)"; 
 } 
 function banana() { 
- 	document.getElementById("banana").innerHTML = "Banana"; 
+ 	document.getElementById("banana").innerHTML = "Banana(banana)"; 
 } 
 function blueberry() { 
- 	document.getElementById("blueberry").innerHTML = "Bluberry"; 
+ 	document.getElementById("blueberry").innerHTML = "Bluberry(bluberi)"; 
 } 
 function broccoli() { 
-	document.getElementById("broccoli").innerHTML = "Broccoli"; 
+	document.getElementById("broccoli").innerHTML = "Broccoli(brokoli)"; 
 } 
 function lettuce() { 
-	document.getElementById("lettuce").innerHTML = "Lettuce"; 
+	document.getElementById("lettuce").innerHTML = "Lettuce(letuce)"; 
 } 
 function cucumbers() { 
- 	document.getElementById("cucumbers").innerHTML = "Cucumbers"; 
+ 	document.getElementById("cucumbers").innerHTML = "Cucumbers(qukumbr)"; 
 } 
 function carrot() { 
-	document.getElementById("carrot").innerHTML = "Carrot"; 
+	document.getElementById("carrot").innerHTML = "Carrot(karot)"; 
 } 
 function pepper() { 
- 	document.getElementById("pepper").innerHTML = "Pepper"; 
+ 	document.getElementById("pepper").innerHTML = "Pepper(pepr)"; 
 } 
 function celery() { 
- 	document.getElementById("celery").innerHTML = "Celery"; 
+ 	document.getElementById("celery").innerHTML = "Celery(seleri)"; 
 } 
 function spinach() { 
-	document.getElementById("spinach").innerHTML = "spinach"; 
+	document.getElementById("spinach").innerHTML = "spinach(spinash)"; 
 } 
 function potatoes() { 
-	document.getElementById("potatoes").innerHTML = "Potatoes"; 
+	document.getElementById("potatoes").innerHTML = "Potatoes(peteito)"; 
 } 
 function watermelon(){
-    document.getElementById("watermelon").innerHTML="Watermelon";
+    document.getElementById("watermelon").innerHTML="Watermelon(watrmelon)";
 }
-
