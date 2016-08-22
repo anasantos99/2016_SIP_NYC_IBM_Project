9ce0e524dbb2e8e5b49fc5fe891e53e69ae6aabb
Cathyln15 <sthmendoza.cat@gmail.com>
2016-08-22 13:41:27 -0400

    Update que.js

--- que.js
+++ que.js
@@ -1,45 +1,45 @@
 function printer() {
-		document.getElementById("printer").innerHTML = "Where is the printer?";
+		document.getElementById("printer").innerHTML = "Where is the printer? (Where is da printer?)";
 	}
 function jefe() {
-	document.getElementById("jefe").innerHTML = "Where is the boss?";
+	document.getElementById("jefe").innerHTML = "Where is the boss? (Where is the boss?)";
 }
 function help() {
-	document.getElementById("help").innerHTML = "Can you help me?";
+	document.getElementById("help").innerHTML = "Can you help me? (Can u help mi?)";
 }
 function think() {
-	document.getElementById("think").innerHTML = "What do you think?";
+	document.getElementById("think").innerHTML = "What do you think? (Wat do you think?)";
 }
 function traducir() {
-	document.getElementById("traducir").innerHTML = "May you please translate that?";
+	document.getElementById("traducir").innerHTML = "May you please translate that? (May you pleaz trans-late dat?)";
 }
 function repeat() {
-	document.getElementById("repeat").innerHTML = "May you please repeat that?";
+	document.getElementById("repeat").innerHTML = "May you please repeat that? (May u please repeat dat?)";
 }
 function plan() {
-	document.getElementById("plan").innerHTML = "What is the plan?";
+	document.getElementById("plan").innerHTML = "What is the plan? (Wat is the plan?)";
 }
 function hora() {
-	document.getElementById("hora").innerHTML = "What time is it?";
+	document.getElementById("hora").innerHTML = "What time is it? (What time is it?)";
 }
 function hoy() {
-	document.getElementById("hoy").innerHTML = "What’s today date?";
+	document.getElementById("hoy").innerHTML = "What’s today date? (Wats tuday date ?)";
 }
 function eso() {
-	document.getElementById("eso").innerHTML = "What is that about?";
+	document.getElementById("eso").innerHTML = "What is that about? (What is that about?)";
 }
 function como() {
-	document.getElementById("como").innerHTML = "How do I do that?";
+	document.getElementById("como").innerHTML = "How do I do that? (How du I du dat?)";
 }
 function algun() {
-	document.getElementById("algun").innerHTML = "Any Advice?";
+	document.getElementById("algun").innerHTML = "Any Advice? (Any ad-vice?)";
 }
 function reunion() {
-	document.getElementById("reunion").innerHTML = "When is the meeting?";
+	document.getElementById("reunion").innerHTML = "When is the meeting? (When is the meeting?)";
 }
 function debe() {
-	document.getElementById("debe").innerHTML = "When is it due?";
+	document.getElementById("debe").innerHTML = "When is it due? (Wen is it due?)";
 }
 function normas() {
-	document.getElementById("normas").innerHTML = "What are the rules? ";
+	document.getElementById("normas").innerHTML = "What are the rules? (Wat are the rules?) ";
 }
