f62073508d7c5f22a96499a5ab52d64a87bbd80a
jami22 <jamiles22@me.com>
2016-08-22 13:50:32 -0400

    added pronunciation

--- bodyparts.js
+++ bodyparts.js
@@ -1,51 +1,51 @@
 function my___hurtsormy___isbroken() {
-		document.getElementById("my___hurtsormy___isbroken").innerHTML = "My___hurts or my___is broken";
+		document.getElementById("my___hurtsormy___isbroken").innerHTML = "My___hurts or my___is broken(mai___hurts or mai_ es brokin)";
 	}
 function hands() {
-	document.getElementById("hands").innerHTML = "Hands";
+	document.getElementById("hands").innerHTML = "Hands(jands)";
 }
 function fingers() {
-	document.getElementById("fingers").innerHTML = "Fingers";
+	document.getElementById("fingers").innerHTML = "Fingers(fengers)";
 }
 function arms() {
-	document.getElementById("arms").innerHTML = "Arms";
+	document.getElementById("arms").innerHTML = "Arms(arms)";
 }
 function neck() {
-	document.getElementById("neck").innerHTML = "Neck";
+	document.getElementById("neck").innerHTML = "Neck(nek)";
 }
 function heads() {
-	document.getElementById("heads").innerHTML = "Head";
+	document.getElementById("heads").innerHTML = "Head(jead)";
 }
 function eyes() {
-	document.getElementById("eyes").innerHTML = "Eyes";
+	document.getElementById("eyes").innerHTML = "Eyes(ais)";
 }
 function nose() {
-	document.getElementById("nose").innerHTML = "Nose";
+	document.getElementById("nose").innerHTML = "Nose(nous)";
 }
 function ears() {
-	document.getElementById("ears").innerHTML = "Ears";
+	document.getElementById("ears").innerHTML = "Ears(ers)";
 }
 function mouth() {
-	document.getElementById("mouth").innerHTML = "Mouth";
+	document.getElementById("mouth").innerHTML = "Mouth(mouth)";
 }
 function legs() {
-	document.getElementById("legs").innerHTML = "Legs";
+	document.getElementById("legs").innerHTML = "Legs(ligs)";
 }
 function feet() {
-		document.getElementById("feet").innerHTML = "Feet";
+		document.getElementById("feet").innerHTML = "Feet(fiit)";
 	}
 function ankles() {
-	document.getElementById("ankles").innerHTML = "Ankles";
+	document.getElementById("ankles").innerHTML = "Ankles(ankels)";
 }
 function knees() {
-	document.getElementById("knees").innerHTML = "Knees";
+	document.getElementById("knees").innerHTML = "Knees(kniis)";
 }
 function back() {
-	document.getElementById("back").innerHTML = "Back";
+	document.getElementById("back").innerHTML = "Back(bak)";
 }
 function stomach() {
-	document.getElementById("stomach").innerHTML = "Stomach";
+	document.getElementById("stomach").innerHTML = "Stomach(stomak)";
 }
 function chest() {
-	document.getElementById("chest").innerHTML = "Chest";
+	document.getElementById("chest").innerHTML = "Chest(chest)";
 }
