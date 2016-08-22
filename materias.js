8765263a94deeb59d8475d5edab2d8e07325fda7
anasantos99 <ana.santos.0818@gmail.com>
2016-08-22 14:13:53 -0400

    Update materias.js

--- materias.js
+++ materias.js
@@ -1,72 +1,72 @@
 function writing() {
-		document.getElementById("writing").innerHTML = "Writing";
+		document.getElementById("writing").innerHTML = "Writing (raitin)";
 	}
 function science() {
-	document.getElementById("science").innerHTML = "Science";
+	document.getElementById("science").innerHTML = "Science (saiens)";
 }
 function history() {
-	document.getElementById("history").innerHTML = "History";
+	document.getElementById("history").innerHTML = "History (jistori)";
 }
 function gym() {
-	document.getElementById("gym").innerHTML = "Physical Education";
+	document.getElementById("gym").innerHTML = "Physical Education (fisicol educashon)";
 }
 function chem() {
-	document.getElementById("chem").innerHTML = "Chemistry";
+	document.getElementById("chem").innerHTML = "Chemistry (quemistri)";
 }
 function art() {
-	document.getElementById("art").innerHTML = "Art";
+	document.getElementById("art").innerHTML = "Art (art)";
 }
 function math() {
-	document.getElementById("math").innerHTML = "Math";
+	document.getElementById("math").innerHTML = "Math (math)";
 }
 function music() {
-	document.getElementById("music").innerHTML = "Music";
+	document.getElementById("music").innerHTML = "Music (miusik)";
 }
 function physics() {
-	document.getElementById("physics").innerHTML = "Physics";
+	document.getElementById("physics").innerHTML = "Physics (fisiks)";
 }
 function bio() {
-	document.getElementById("bio").innerHTML = "Biology";
+	document.getElementById("bio").innerHTML = "Biology (baiolochi)";
 }
 function earth() {
-	document.getElementById("earth").innerHTML = "Earth Science";
+	document.getElementById("earth").innerHTML = "Earth Science (urth saiens)";
 }
 function astro() {
-	document.getElementById("astro").innerHTML = "Astronomy";
+	document.getElementById("astro").innerHTML = "Astronomy (astronomi)";
 }
 function italian() {
-	document.getElementById("italian").innerHTML = "Italian";
+	document.getElementById("italian").innerHTML = "Italian (italien)";
 }
 function french() {
-	document.getElementById("french").innerHTML = "French";
+	document.getElementById("french").innerHTML = "French (french)";
 }
 function chinese() {
-	document.getElementById("chinese").innerHTML = "Chinese";
+	document.getElementById("chinese").innerHTML = "Chinese (chainis)";
 }
 function greek() {
-	document.getElementById("greek").innerHTML = "Greek";
+	document.getElementById("greek").innerHTML = "Greek (grik)";
 }
 function geo() {
-	document.getElementById("geo").innerHTML = "Geometry";
+	document.getElementById("geo").innerHTML = "Geometry (chiometri)";
 }
 function ss() {
-	document.getElementById("ss").innerHTML = "Social Studies";
+	document.getElementById("ss").innerHTML = "Social Studies (soshial stodis)";
 }
 function lit() {
-	document.getElementById("lit").innerHTML = "English Literature";
+	document.getElementById("lit").innerHTML = "English Literature (inglish literachure)";
 }
 function sports() {
-	document.getElementById("sports").innerHTML = "Sports";
+	document.getElementById("sports").innerHTML = "Sports (sports)";
 }
 function latin() {
-	document.getElementById("latin").innerHTML = "Latin";
+	document.getElementById("latin").innerHTML = "Latin (latin)";
 }
 function calc() {
-	document.getElementById("calc").innerHTML = "Calculus";
+	document.getElementById("calc").innerHTML = "Calculus (calciulos)";
 }
 function trig() {
-	document.getElementById("trig").innerHTML = "Trigonometry";
+	document.getElementById("trig").innerHTML = "Trigonometry (trigonometri)";
 }
 function honors() {
-	document.getElementById("honors").innerHTML = "Honors Class";
+	document.getElementById("honors").innerHTML = "Honors Class (onors clas)";
 }
