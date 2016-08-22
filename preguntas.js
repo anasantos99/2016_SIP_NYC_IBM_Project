77389d591a4be1af542a569a627f313872e2088b
anasantos99 <ana.santos.0818@gmail.com>
2016-08-22 14:17:38 -0400

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,18 +1,18 @@
 function behavior() {
-	document.getElementById("behavior").innerHTML = "How is my child behaving?";
+	document.getElementById("behavior").innerHTML = "How is my child behaving? (jao is mai chaild bijeivin)";
 }
 function hw() {
-	document.getElementById("hw").innerHTML = "Has my child given in all of the homework assignments?";
+	document.getElementById("hw").innerHTML = "Has my child given in all of the homework assignments? (jas mai chaild given in ol ov deur jomwerk)";
 }
 function problem() {
-	document.getElementById("problem").innerHTML = "Is there a problem with my child in class?";
+	document.getElementById("problem").innerHTML = "Is there a problem with my child in class? (is deur a problom uith mai chaild in clas)";
 }
 function extra() {
-	document.getElementById("extra").innerHTML = "Is there something my child can do to improve their grade?";
+	document.getElementById("extra").innerHTML = "Is there something my child can do to improve their grade? (is deur somthin mai chaild can du tu impruv deur graid";
 }
 function repeat() {
-	document.getElementById("repeat").innerHTML = "Could you repeat the question?";
+	document.getElementById("repeat").innerHTML = "Could you repeat the question? (cud yiu ripit de cueschon)";
 }
 function home() {
-	document.getElementById("home").innerHTML = "How can I help my child at home?";
+	document.getElementById("home").innerHTML = "How can I help my child at home? (jao can ai jelp mai chaild at jom)";
 }
