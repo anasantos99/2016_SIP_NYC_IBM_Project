d0a55848eb6195b43e1056c3b7474569b615a837
anasantos99 <ana.santos.0818@gmail.com>
2016-08-22 14:13:19 -0400

    Update grades.js

--- grades.js
+++ grades.js
@@ -1,30 +1,30 @@
 function pass() {
-	document.getElementById("pass").innerHTML = "Pass the Class";
+	document.getElementById("pass").innerHTML = "Pass the Class (pas de clas)";
 }
 function fail() {
-	document.getElementById("fail").innerHTML = "Fail the class";
+	document.getElementById("fail").innerHTML = "Fail the class (feil de clas)";
 }
 function excellent() {
-	document.getElementById("excellent").innerHTML = "Excellente";
+	document.getElementById("excellent").innerHTML = "Excellent (exselent)";
 }
 function great() {
-	document.getElementById("great").innerHTML = "Very Good";
+	document.getElementById("great").innerHTML = "Very Good (veri gud)";
 }
 function poor() {
-	document.getElementById("poor").innerHTML = "Poor";
+	document.getElementById("poor").innerHTML = "Poor (por)";
 }
 function average() {
-	document.getElementById("average").innerHTML = "Average";
+	document.getElementById("average").innerHTML = "Average (averech)";
 }
 function improve() {
-	document.getElementById("improve").innerHTML = "Needs Improvement";
+	document.getElementById("improve").innerHTML = "Needs Improvement (nids impruvment)";
 }
 function progress() {
-	document.getElementById("progress").innerHTML = "Good Progress";
+	document.getElementById("progress").innerHTML = "Good Progress (gud progres)";
 }
 function danger() {
-	document.getElementById("danger").innerHTML = "Indanger of failing the class";
+	document.getElementById("danger").innerHTML = "In danger of failing the class (in dencher ov  feilin de clas)";
 }
 function advanced() {
-	document.getElementById("advanced").innerHTML = "Is ahead of the rest of the class";
+	document.getElementById("advanced").innerHTML = "Is ahead of the rest of the class (is ajed ov the rest ov de clas)";
 }
