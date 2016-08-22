27a8c1d09eb93b86b6ec66a905da62a022c5fe78
Cathyln15 <sthmendoza.cat@gmail.com>
2016-08-22 13:19:23 -0400

    Update words.js

--- words.js
+++ words.js
@@ -2,53 +2,53 @@ function si() {
 		document.getElementById("si").innerHTML = "Yes (ies)";
 	}
 function no() {
-	document.getElementById("no").innerHTML = "No";
+	document.getElementById("no").innerHTML = "No (No)";
 }
 function mon() {
-	document.getElementById("mon").innerHTML = "Monday";
+	document.getElementById("mon").innerHTML = "Monday (Mondai)";
 }
 function tues() {
-	document.getElementById("tues").innerHTML = "Tuesday";
+	document.getElementById("tues").innerHTML = "Tuesday (Tuesdai)";
 }
 function mi() {
-	document.getElementById("mi").innerHTML = "Wendnesday";
+	document.getElementById("mi").innerHTML = "Wendnesday (Wensdai)";
 }
 function hi() {
-	document.getElementById("hi").innerHTML = "Hello";
+	document.getElementById("hi").innerHTML = "Hello (Hello)";
 }
 function what() {
-	document.getElementById("what").innerHTML = "What?";
+	document.getElementById("what").innerHTML = "What? (Wat)";
 }
 function ju() {
-	document.getElementById("ju").innerHTML = "Thursday";
+	document.getElementById("ju").innerHTML = "Thursday (Thursdai)";
 }
 function frid() {
-	document.getElementById("frid").innerHTML = "Friday ";
+	document.getElementById("frid").innerHTML = "Friday (Fridai) ";
 }
 function saba() {
-	document.getElementById("saba").innerHTML = "Saturday";
+	document.getElementById("saba").innerHTML = "Saturday (Saturdai)";
 }
 function domi() {
-	document.getElementById("domi").innerHTML = "Sunday";
+	document.getElementById("domi").innerHTML = "Sunday (Sundai)";
 }
 function office() {
-	document.getElementById("office").innerHTML = "Office";
+	document.getElementById("office").innerHTML = "Office (Office)";
 }
 function cost() {
-	document.getElementById("cost").innerHTML = "Estimate";
+	document.getElementById("cost").innerHTML = "Estimate (Estimate)";
 }
 function deposit() {
 	document.getElementById("deposit").innerHTML = "To deposit";
 }
 function salir() {
-	document.getElementById("salir").innerHTML = "   Exit";
+	document.getElementById("salir").innerHTML = "   Exit (Exit)";
 }
 function pencil() {
-	document.getElementById("pencil").innerHTML = "Pencil";
+	document.getElementById("pencil").innerHTML = "Pencil (Pensil)";
 	
 }
 function pen() {
-	document.getElementById("pen").innerHTML = "Pen";
+	document.getElementById("pen").innerHTML = "Pen (Pen)";
 	
 }
 function papel() {
@@ -56,19 +56,20 @@ function papel() {
 	
 }
 function table() {
-	document.getElementById("table").innerHTML = "Table";
+	document.getElementById("table").innerHTML = "Table (Table)";
 	
 }
 function way() {
-	document.getElementById("way").innerHTML = "Hallway";
+	document.getElementById("way").innerHTML = "Hallway (Hal-way)";
 	
 }
 function stairs() {
-	document.getElementById("stairs").innerHTML = "Stairs";
+	document.getElementById("stairs").innerHTML = "Stairs (Sta-irs)";
 	
 }
 function room() {
-	document.getElementById("room").innerHTML = "Bathroom";
+	document.getElementById("room").innerHTML = "Bathroom 
+	";
 	
 }
 function roof() {
@@ -76,7 +77,7 @@ function roof() {
 	
 }
 function garg() {
-	document.getElementById("garg").innerHTML = "Garbage";
+	document.getElementById("garg").innerHTML = "Garbage (Garb-age)";
 	
 }
 function weather() {
@@ -84,19 +85,19 @@ function weather() {
 	
 }
 function room() {
-	document.getElementById("room").innerHTML = "Bathroom";
+	document.getElementById("room").innerHTML = "Bathroom (Bath-rum)";
 	
 }
 function door() {
-	document.getElementById("door").innerHTML = "Door";
+	document.getElementById("door").innerHTML = "Door (Dor)";
 	
 }
 function restu() {
-	document.getElementById("restu").innerHTML = "Resturant ";
+	document.getElementById("restu").innerHTML = "Resturant (Rest-urant) ";
 	
 }
 function store() {
-	document.getElementById("store").innerHTML = "Store ";
+	document.getElementById("store").innerHTML = "Store (Sto-re) ";
 	
 }
 function food() {
