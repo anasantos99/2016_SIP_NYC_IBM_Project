24e7ea1bc18e5fc38999bd1fa12ea3b810e4e59c
jami22 <jamiles22@me.com>
2016-08-22 13:47:04 -0400

    added pronunciation

--- questions.js
+++ questions.js
@@ -1,24 +1,24 @@
 function why() {
-		document.getElementById("why").innerHTML = "Why are you here today?";
+		document.getElementById("why").innerHTML = "Why are you here today?(uay ar u jere today?)";
 	}
 function how() {
-	document.getElementById("how").innerHTML = "How are you feeling?";
+	document.getElementById("how").innerHTML = "How are you feeling?(jao ar you feeling?)";
 }
 function have() {
-	document.getElementById("have").innerHTML = "Have you had any pain lately?";
+	document.getElementById("have").innerHTML = "Have you had any pain lately?(jaf u jad ane pein leitli?)";
 }
 function when() {
-	document.getElementById("when").innerHTML = "When did this start?";
+	document.getElementById("when").innerHTML = "When did this start?(uen did dis start?)";
 }
 function at() {
-	document.getElementById("at").innerHTML = "What time do you go to sleep?";
+	document.getElementById("at").innerHTML = "What time do you go to sleep?(uat time du u go tu sliip?)";
 }
 function what() {
-	document.getElementById("what").innerHTML = "What do you do on a daily basis?";
+	document.getElementById("what").innerHTML = "What do you do on a daily basis?(uat du u du on a daili basis?)";
 }
 function diet() {
-	document.getElementById("diet").innerHTML = "What does your diet look like?";
+	document.getElementById("diet").innerHTML = "What does your diet look like?(uat does your diet lok like?)";
 }
 function exercise() {
-	document.getElementById("exercise").innerHTML = "What is your exercise routine?";
+	document.getElementById("exercise").innerHTML = "What is your exercise routine?(uat is your exercise rotine?)";
 }
