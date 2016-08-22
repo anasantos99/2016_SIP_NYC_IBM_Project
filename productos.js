a1a0fe5dc82162f10157394ee3060548be029888
vanessamendoza <vanessam363@gmail.com>
2016-08-22 11:31:43 -0400

    Update productos.js
    added pronunciation for the words in english

--- productos.js
+++ productos.js
@@ -1,57 +1,57 @@
 function milk() {
-		document.getElementById("milk").innerHTML = "Milk";
+		document.getElementById("milk").innerHTML = "Milk(milck)";
 	}
 function cheese() {
-	document.getElementById("cheese").innerHTML = "Cheese";
+	document.getElementById("cheese").innerHTML = "Cheese(chis)";
 }
 function soap() {
-	document.getElementById("soap").innerHTML = "Soap";
+	document.getElementById("soap").innerHTML = "Soap(sop)";
 }
 function salt() {
-	document.getElementById("salt").innerHTML = "Salt";
+	document.getElementById("salt").innerHTML = "Salt(solt)";
 }
 function meat() {
-	document.getElementById("meat").innerHTML = "Meat";
+	document.getElementById("meat").innerHTML = "Meat(mit)";
 }
 function chicken() {
-	document.getElementById("chicken").innerHTML = "Chicken";
+	document.getElementById("chicken").innerHTML = "Chicken(cheken)";
 }
 function yogurt() {
-	document.getElementById("yogurt").innerHTML = "Yogurt";
+	document.getElementById("yogurt").innerHTML = "Yogurt(Jogur)";
 }
 function bread() {
-	document.getElementById("bread").innerHTML = "Bread";
+	document.getElementById("bread").innerHTML = "Bread(bred)";
 }
 function coffee() {
-	document.getElementById("coffee").innerHTML = "Coffee";
+	document.getElementById("coffee").innerHTML = "Coffee(cofe)";
 }
 function juice() {
-	document.getElementById("juice").innerHTML = "Juice";
+	document.getElementById("juice").innerHTML = "Juice(juse)";
 }
 function water() {
-	document.getElementById("water").innerHTML = "Water";
+	document.getElementById("water").innerHTML = "Water(watr)";
 }
 function tea() {
-	document.getElementById("tea").innerHTML = "Tea";
+	document.getElementById("tea").innerHTML = "Tea(ti)";
 }
 function sugar() {
-	document.getElementById("sugar").innerHTML = "Sugar";
+	document.getElementById("sugar").innerHTML = "Sugar(shugar)";
 }
 function rice() {
-	document.getElementById("rice").innerHTML = "Rice";
+	document.getElementById("rice").innerHTML = "Rice(rise)";
 }
 function oil() {
-	document.getElementById("oil").innerHTML = "Oil";
+	document.getElementById("oil").innerHTML = "Oil(oyl)";
 }
 function oatmeal() {
-	document.getElementById("oatmeal").innerHTML = "Oatmeal";
+	document.getElementById("oatmeal").innerHTML = "Oatmeal(otmeil)";
 }
 function napkins() {
-	document.getElementById("napkins").innerHTML = "Napkins";
+	document.getElementById("napkins").innerHTML = "Napkins(nabkins)";
 }
 function eggs() {
-	document.getElementById("eggs").innerHTML = "Eggs";
+	document.getElementById("eggs").innerHTML = "Eggs(egs)";
 }
 function fish() {
-	document.getElementById("fish").innerHTML = "Fish";
+	document.getElementById("fish").innerHTML = "Fish(fesh)";
 }
