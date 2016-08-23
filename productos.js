c899f11b91f06ec08f922ce58cf5a1ccca0f9b85
vanessamendoza <vanessam363@gmail.com>
2016-08-23 09:09:12 -0400

    Update productos.js
    added the if and else statements for the productos.js file

--- productos.js
+++ productos.js
@@ -1,57 +1,159 @@
 function milk() {
-		document.getElementById("milk").innerHTML = "Milk(milck)";
-	}
+    if(document.getElementById("milk").innerHTML == "Milk(milck)"){
+        document.getElementById("milk").innerHTML=" ";
+    } else {
+        document.getElementById("milk").innerHTML = "Milk(milck)";
+    }
+		
+}
+    
 function cheese() {
-	document.getElementById("cheese").innerHTML = "Cheese(chis)";
+    if(document.getElementById("cheese").innerHTML == "Cheese(chis)"){
+        document.getElementById("cheese").innerHTML=" ";
+    } else {
+        document.getElementById("cheese").innerHTML = "Cheese(chis)";
+    }
+		
 }
+	
+
 function soap() {
+    if(document.getElementById("soap").innerHTML=="Soap(sop)"){
+        document.getElementById("soap").innerHTML="";
+    }else{
 	document.getElementById("soap").innerHTML = "Soap(sop)";
+    }
 }
+
 function salt() {
+	 if(document.getElementById("salt").innerHTML=="Salt(solt)"){
+        document.getElementById("salt").innerHTML="";
+    }else{
 	document.getElementById("salt").innerHTML = "Salt(solt)";
+    }
 }
+    
 function meat() {
+	 if(document.getElementById("meat").innerHTML=="Meat(mit)"){
+        document.getElementById("meat").innerHTML="";
+    }else{
 	document.getElementById("meat").innerHTML = "Meat(mit)";
+    }
 }
+	
+
 function chicken() {
+     if(document.getElementById("chicken").innerHTML=="Chicken(cheken)"){
+        document.getElementById("chicken").innerHTML="";
+    }else{
 	document.getElementById("chicken").innerHTML = "Chicken(cheken)";
+    }
 }
+	
+
 function yogurt() {
+     if(document.getElementById("yogurt").innerHTML=="Yogurt(Jogur)"){
+        document.getElementById("yogurt").innerHTML="";
+    }else{
 	document.getElementById("yogurt").innerHTML = "Yogurt(Jogur)";
+    }
 }
+	
+	
 function bread() {
+    if(document.getElementById("bread").innerHTML=="Bread(bred)"){
+        document.getElementById("bread").innerHTML="";
+    }else{
 	document.getElementById("bread").innerHTML = "Bread(bred)";
+    }
 }
 function coffee() {
+    if(document.getElementById("coffee").innerHTML=="Coffee(cofe)"){
+        document.getElementById("coffee").innerHTML="";
+    }else{
 	document.getElementById("coffee").innerHTML = "Coffee(cofe)";
+    }
 }
+	
 function juice() {
+     if(document.getElementById("juice").innerHTML=="Juice(juse)"){
+        document.getElementById("juice").innerHTML="";
+    }else{
 	document.getElementById("juice").innerHTML = "Juice(juse)";
+    }
 }
+
 function water() {
+     if(document.getElementById("water").innerHTML=="Water(watr)"){
+        document.getElementById("water").innerHTML="";
+    }else{
 	document.getElementById("water").innerHTML = "Water(watr)";
+    }
 }
+	
 function tea() {
+    if(document.getElementById("tea").innerHTML=="Tea(ti)"){
+        document.getElementById("tea").innerHTML="";
+    }else{
 	document.getElementById("tea").innerHTML = "Tea(ti)";
+    }
 }
+
 function sugar() {
+        if(document.getElementById("sugar").innerHTML=="Sugar(shugar)"){
+        document.getElementById("sugar").innerHTML="";
+    }else{
 	document.getElementById("sugar").innerHTML = "Sugar(shugar)";
+    }
 }
+
 function rice() {
+    if(document.getElementById("rice").innerHTML=="Rice(rise)"){
+        document.getElementById("rice").innerHTML="";
+    }else{
 	document.getElementById("rice").innerHTML = "Rice(rise)";
+    }
 }
+
+
 function oil() {
+    if(document.getElementById("oil").innerHTML=="Oil(oyl)"){
+        document.getElementById("oil").innerHTML="";
+    }else{
 	document.getElementById("oil").innerHTML = "Oil(oyl)";
+    }
 }
+
 function oatmeal() {
+     if(document.getElementById("oatmeal").innerHTML=="Oatmeal(otmeil)"){
+        document.getElementById("oatmeal").innerHTML="";
+    }else{
 	document.getElementById("oatmeal").innerHTML = "Oatmeal(otmeil)";
+    }
 }
+
 function napkins() {
+     if(document.getElementById("napkins").innerHTML=="Napkins(nabkins)"){
+        document.getElementById("napkins").innerHTML="";
+    }else{
 	document.getElementById("napkins").innerHTML = "Napkins(nabkins)";
+    }
 }
+
+
 function eggs() {
+     if(document.getElementById("eggs").innerHTML=="Eggs(egs)"){
+        document.getElementById("eggs").innerHTML="";
+    }else{
 	document.getElementById("eggs").innerHTML = "Eggs(egs)";
+    }
 }
+	
 function fish() {
+    if(document.getElementById("fish").innerHTML=="Fish(fesh)"){
+        document.getElementById("fish").innerHTML="";
+    }else{
 	document.getElementById("fish").innerHTML = "Fish(fesh)";
+    }
 }
+	
