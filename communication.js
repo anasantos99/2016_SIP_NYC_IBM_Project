38ceaa4b0a17fa01954c2ef3e3dc4cb982c4a553
Cathyln15 <sthmendoza.cat@gmail.com>
2016-08-23 09:37:01 -0400

    Update communication.js

--- communication.js
+++ communication.js
@@ -1,33 +1,77 @@
 function dias() {
-		document.getElementById("dias").innerHTML = "GoodMorning (Gud Mourning)";
+	if(document.getElementById("dias").innerHTML == "GoodMorning (Gud Mourning)"){
+		document.getElementById("dias").innerHTML = "";
+	} else{
+	document.getElementById("dias").innerHTML = "GoodMorning (Gud Mourning)";
 	}
+}
 function dia() {
+	if(document.getElementById("dia").innerHTML == "Good Afternoon (Gud Afternun)"){
+		document.getElementById("dia").innerHTML = "";
+	} else{
 	document.getElementById("dia").innerHTML = "Good Afternoon (Gud Afternun)";
+	}
 }
 function night() {
+	if (document.getElementById("night").innerHTML == "Good Night (Gud Nait)"){
+		document.getElementById("night").innerHTML = "";
+	} else{
 	document.getElementById("night").innerHTML = "Good Night (Gud Nait)";
+	}
 }
 function gusto() {
-	document.getElementById("gusto").innerHTML = "Nice to meet you (Naise tu mit iu)";
+	if(document.getElementById("gusto").innerHTML == "Nice to meet you (Naise tu mit iu)"){
+		document.getElementById("gusto").innerHTML="";
+	} else{
+		document.getElementById("gusto").innerHTML="Nice to meet you (Naise tu mit iu)";
+	}
 }
 function thnxz() {
-	document.getElementById("thnxz").innerHTML = "Thank You (thank u)";
+	if(document.getElementById("thnxz").innerHTML == "Thank You (thank u)"){
+	document.getElementById("thnxz").innerHTML="";
+	} else{
+		document.getElementById("thnxz").innerHTML="Thank You (thank u)";
+	}
 }
 function sorry() {
-	document.getElementById("sorry").innerHTML = "Im sorry (aim sorri))";
+	if (document.getElementById("sorry").innerHTML == "Im sorry (aim sorri)"){
+		document.getElementById("sorry").innerHTML="";
+	} else{
+		document.getElementById("sorry").innerHTML="Im sorry (aim sorri)";
+	}
 }
 function me() {
-	document.getElementById("me").innerHTML = "Excuse Me (Excius mi))";
+	if (document.getElementById("me").innerHTML == "Excuse Me (Excius mi)"){
+		document.getElementById("me").innerHTML="";
+	} else{
+		document.getElementById("me").innerHTML="Excuse Me (Excius mi)";
+	}
 }
 function please() {
-	document.getElementById("please").innerHTML = "Please (Pliz)";
+	if (document.getElementById("please").innerHTML == "Please (Pliz)"){
+		document.getElementById("please").innerHTML="";
+	} else{
+		document.getElementById("please").innerHTML="Please (Pliz)";
+	}
 }
 function you() {
-	document.getElementById("you").innerHTML = "Bless You (Bless iu)";
+	if (document.getElementById("you").innerHTML == "Bless You (Bless iu)"){
+		document.getElementById("you").innerHTML="";
+	} else{
+		document.getElementById("you").innerHTML="Bless You (Bless iu)";
+	}
 }
 function welcome() {
-	document.getElementById("welcome").innerHTML = "You are Welcome (iu r uel-com)";
+	if (document.getElementById("welcome").innerHTML == "You are Welcome (iu r uel-com)"){
+		document.getElementById("welcome").innerHTML="";
+	} else{
+		document.getElementById("welcome").innerHTML="You are Welcome (iu r uel-com)";
+	}
 }
-function i() {
-	document.getElementById("i").innerHTML = "I do not understand (ai du nat under-stan)";
+function comp() {
+	if (document.getElementById("comp").innerHTML == "I do not understand (ai du nat under-stan)"){
+		document.getElementById("comp").innerHTML="";
+	} else{
+		document.getElementById("comp").innerHTML="I do not understand (ai du nat under-stan)";
+	}
 }
