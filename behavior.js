4bac949ab4593cca7b27d5e511244b8ef1fd3145
anasantos99 <ana.santos.0818@gmail.com>
2016-08-23 10:39:24 -0400

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,36 +1,84 @@
 function disrespectful() {
+	if(document.getElementById("disrespectful").innerHTML == "Disrespectful (disrispectful)"){
+		document.getElementById("disrespectful").innerHTML = "";
+	} else {
 	document.getElementById("disrespectful").innerHTML = "Disrespectful (disrispectful)";
+	}
 }
 function ignorant() {
+	if(document.getElementById("ignorant").innerHTML == "Ignorant (ignorent)"){
+		document.getElementById("ignorant").innerHTML = "";
+	} else {
 	document.getElementById("ignorant").innerHTML = "Ignorant (ignorent)";
+	}
 }
 function attention() {
-	document.getElementById("attention").innerHTML = "Does not pay attention (dos nat pei atenchon)";
+	if(document.getElementById("attention").innerHTML == "Does not pay attention (attention)"){
+		document.getElementById("attention").innerHTML = "";
+	} else {
+	document.getElementById("attention").innerHTML = "Does not pay attention (attention)";
+	}
 }
 function talk() {
-	document.getElementById("talk").innerHTML = "Talks a lot (talks a lot)";
+	if(document.getElementById("talk").innerHTML == "Talks a lot (talks a lot)"){
+		document.getElementById("talk").innerHTML = "";
+	} else {
+	document.getElementById("talk").innerHTML = "Talks a lot (talks a lot )";
+	}
 }
 function quiet() {
+	if(document.getElementById("quiet").innerHTML == "Quiet (cuaiet)"){
+		document.getElementById("quiet").innerHTML = "";
+	} else {
 	document.getElementById("quiet").innerHTML = "Quiet (cuaiet)";
+	}
 }
 function shy() {
+	if(document.getElementById("shy").innerHTML == "Shy (shai)"){
+		document.getElementById("shy").innerHTML = "";
+	} else {
 	document.getElementById("shy").innerHTML = "Shy (shai)";
+	}
 }
 function phone() {
-	document.getElementById("phone").innerHTML = "Always on their phone (oluais on deur fon)";
+	if(document.getElementById("phone").innerHTML == "Always on their phone (ouluais on deur fon)"){
+		document.getElementById("phone").innerHTML = "";
+	} else {
+	document.getElementById("phone").innerHTML = "Always on their phone (ouluais on deur fon)";
+	}
 }
 function participate() {
+	if(document.getElementById("participate").innerHTML == "Needs to participate more during class (nids tu partisipait mor durin clas)"){
+		document.getElementById("participate").innerHTML = "";
+	} else {
 	document.getElementById("participate").innerHTML = "Needs to participate more during class (nids tu partisipait mor durin clas)";
+	}
 }
 function skip() {
+	if(document.getElementById("skip").innerHTML == "Does not come to class (doe nat com tu clas)"){
+		document.getElementById("skip").innerHTML = "";
+	} else {
 	document.getElementById("skip").innerHTML = "Does not come to class (doe nat com tu clas)";
+	}
 }
 function good() {
+	if(document.getElementById("good").innerHTML == "Very well behaved (veri uel bijaivd)"){
+		document.getElementById("good").innerHTML = "";
+	} else {
 	document.getElementById("good").innerHTML = "Very well behaved (veri uel bijaivd)";
+	}
 }
 function help() {
+	if(document.getElementById("help").innerHTML == "Helps their classmates (jelps deur clasmaits)"){
+		document.getElementById("help").innerHTML = "";
+	} else {
 	document.getElementById("help").innerHTML = "Helps their classmates (jelps deur clasmaits)";
+	}
 }
 function work() {
-	document.getElementById("work").innerHTML = "Very Hardworking (veri jard  uorkin)";
+	if(document.getElementById("work").innerHTML == "Very Hardworking (veri jard uorkin)"){
+		document.getElementById("work").innerHTML = "";
+	} else {
+	document.getElementById("work").innerHTML = "Very Hardworking (veri jard uorkin)";
+	}
 }
