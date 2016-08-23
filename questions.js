6e095b8ad399334e93042143de46908da2e17fe4
jami22 <jamiles22@me.com>
2016-08-23 09:33:32 -0400

    update

--- questions.js
+++ questions.js
@@ -1,24 +1,57 @@
 function why() {
-		document.getElementById("why").innerHTML = "Why are you here today?(uay ar u jere today?)";
+		if(document.getElementById("why").innerHTML == "Why are you here today?(uay ar u jere today?)"){
+            document.getElementById("why").innerHTML = "";
+        } else {
+            document.getElementById("why").innerHTML = "Why are you here today?(uay ar u jere today?)";
+        }
 	}
 function how() {
-	document.getElementById("how").innerHTML = "How are you feeling?(jao ar you feeling?)";
+	if(document.getElementById("how").innerHTML == "How are you feeling?(jao ar you feeling?)"){
+        document.getElementById("how").innerHTML = "";
+    } else {
+        document.getElementById("how").innerHTML = "How are you feeling?(jao ar you feeling?)";
+    }
 }
 function have() {
-	document.getElementById("have").innerHTML = "Have you had any pain lately?(jaf u jad ane pein leitli?)";
+	if(document.getElementById("have").innerHTML == "Have you had any pain lately?(jaf u jad ane pein leitli?)"){
+        document.getElementById("have").innerHTML = "";
+    } else {
+        document.getElementById("have").innerHTML = "Have you had any pain lately?(jaf u jad ane pein leitli?)";
+    }
 }
 function when() {
-	document.getElementById("when").innerHTML = "When did this start?(uen did dis start?)";
+	if(document.getElementById("when").innerHTML == "When did this start?(uen did dis start?)"){
+        document.getElementById("when").innerHTML = "";
+    } else {
+        document.getElementById("when").innerHTML = "When did this start?(uen did dis start?)";
+    }
 }
 function at() {
-	document.getElementById("at").innerHTML = "What time do you go to sleep?(uat time du u go tu sliip?)";
+	if(document.getElementById("at").innerHTML == "What time do you go to sleep?(uat time du u go tu sliip?)"){
+        document.getElementById("at").innerHTML = "";
+    } else {
+        document.getElementById("at").innerHTML = "What time do you go to sleep?(uat time du u go tu sliip?)";
+    }
 }
 function what() {
-	document.getElementById("what").innerHTML = "What do you do on a daily basis?(uat du u du on a daili basis?)";
+	if(document.getElementById("what").innerHTML == "What do you do on a daily basis?(uat du u du on a daili basis?)"){
+        document.getElementById("what").innerHTML = "";
+    } else {
+        document.getElementById("what").innerHTML = "What do you do on a daily basis?(uat du u du on a daili basis?)";
+    }
 }
 function diet() {
-	document.getElementById("diet").innerHTML = "What does your diet look like?(uat does your diet lok like?)";
+	if(document.getElementById("diet").innerHTML == "What does your diet look like?(uat does your diet lok like?)"){
+        document.getElementById("diet").innerHTML = "";
+    } else {
+        document.getElementById("diet").innerHTML = "What does your diet look like?(uat does your diet lok like?)";
+    }
 }
 function exercise() {
-	document.getElementById("exercise").innerHTML = "What is your exercise routine?(uat is your exercise rotine?)";
+	if(document.getElementById("exercise").innerHTML == "What is your exercise routine?(uat is your exercise rotine?)"){
+        document.getElementById("exercise").innerHTML = "";
+    } else {
+        document.getElementById("exercise").innerHTML = "What is your exercise routine?(uat is your exercise rotine?)";
+    }
 }
+
