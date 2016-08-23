4b977433eedbe0c598051ce7c22112b5794eb685
vanessamendoza <vanessam363@gmail.com>
2016-08-23 09:16:50 -0400

    Update ayuda.js
    added the if and else statements for the ayuda.js file

--- ayuda.js
+++ ayuda.js
@@ -1,12 +1,36 @@
+
 function where() {
-	document.getElementById("where").innerHTML = "Where is the....?(wer is da)";
+    if(document.getElementById("where").innerHTML == "Where is the....?(wer is da)"){
+        document.getElementById("where").innerHTML=" ";
+    } else {
+        document.getElementById("where").innerHTML = "Where is the....?(wer is da)";
+    }
+		
 }
 function the() {
-	document.getElementById("the").innerHTML = "Where are the....?(wer r da)";
+    if(document.getElementById("the").innerHTML == "Where are the....?(wer ar da)"){
+        document.getElementById("the").innerHTML=" ";
+    } else {
+        document.getElementById("the").innerHTML = "Where are the....?(wer ar da)";
+    }
+		
 }
+	
 function how() {
-	document.getElementById("how").innerHTML = "How much?(hou mach?)";
+     if(document.getElementById("how").innerHTML == "How much?(hou mach?)"){
+        document.getElementById("how").innerHTML=" ";
+    } else {
+        document.getElementById("how").innerHTML = "How much?(hou mach?)";
+    }
+		
 }
+	
 function pay() {
-	document.getElementById("pay").innerHTML = "Where can I pay?(were ken I pey?)";
+     if(document.getElementById("pay").innerHTML == "Where can I pay?(were ken I pey?)"){
+        document.getElementById("pay").innerHTML=" ";
+    } else {
+        document.getElementById("pay").innerHTML = "Where can I pay?(were ken I pey?)";
+    }
+		
 }
+	
