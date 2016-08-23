68522b9817fee7efd30b11198ec30b0e0a041a37
anasantos99 <ana.santos.0818@gmail.com>
2016-08-23 10:37:26 -0400

    added code for translation to hide

--- materias.js
+++ materias.js
@@ -1,72 +1,168 @@
 function writing() {
-		document.getElementById("writing").innerHTML = "Writing (raitin)";
+	if(document.getElementById("writing").innerHTML == "Writing (raiting)"){
+		document.getElementById("writing").innerHTML = "";
+	} else {
+	document.getElementById("writing").innerHTML = "Writing (raiting)";
 	}
-function science() {
-	document.getElementById("science").innerHTML = "Science (saiens)";
 }
-function history() {
-	document.getElementById("history").innerHTML = "History (jistori)";
+function science() {
+	if(document.getElementById("science").innerHTML == "Science (saiens)"){
+		document.getElementById("science").innerHTML = "";
+	} else {
+	document.getElementById("science").innerHTML = "Science (saiens)";
+	}
+}
+function story() {
+	if(document.getElementById("story").innerHTML == "History (jistori)"){
+		document.getElementById("story").innerHTML = "";
+	} else {
+	document.getElementById("story").innerHTML = "History (jistori)";
+	}
 }
 function gym() {
+	if(document.getElementById("gym").innerHTML == "Physical Education (fisicol educashon)"){
+		document.getElementById("gym").innerHTML = "";
+	} else {
 	document.getElementById("gym").innerHTML = "Physical Education (fisicol educashon)";
+	}
 }
 function chem() {
+	if(document.getElementById("chem").innerHTML == "Chemistry (quemistri)"){
+		document.getElementById("chem").innerHTML = "";
+	} else {
 	document.getElementById("chem").innerHTML = "Chemistry (quemistri)";
+	}
 }
 function art() {
+	if(document.getElementById("art").innerHTML == "Art (art)"){
+		document.getElementById("art").innerHTML = "";
+	} else {
 	document.getElementById("art").innerHTML = "Art (art)";
+	}
 }
 function math() {
+	if(document.getElementById("math").innerHTML == "Math (math)"){
+		document.getElementById("math").innerHTML = "";
+	} else {
 	document.getElementById("math").innerHTML = "Math (math)";
+	}
 }
 function music() {
+	if(document.getElementById("music").innerHTML == "Music (miusik)"){
+		document.getElementById("music").innerHTML = "";
+	} else {
 	document.getElementById("music").innerHTML = "Music (miusik)";
+	}
 }
 function physics() {
+	if(document.getElementById("physics").innerHTML == "Physics (fisiks)"){
+		document.getElementById("physics").innerHTML = "";
+	} else {
 	document.getElementById("physics").innerHTML = "Physics (fisiks)";
+	}
 }
 function bio() {
+	if(document.getElementById("bio").innerHTML == "Biology (baiolochi)"){
+		document.getElementById("bio").innerHTML = "";
+	} else {
 	document.getElementById("bio").innerHTML = "Biology (baiolochi)";
+	}
 }
 function earth() {
+	if(document.getElementById("earth").innerHTML == "Earth Science (urth saiens)"){
+		document.getElementById("earth").innerHTML = "";
+	} else {
 	document.getElementById("earth").innerHTML = "Earth Science (urth saiens)";
+	}
 }
 function astro() {
+	if(document.getElementById("astro").innerHTML == "Astronomy (astronomi)"){
+		document.getElementById("astro").innerHTML = "";
+	} else {
 	document.getElementById("astro").innerHTML = "Astronomy (astronomi)";
+	}
 }
 function italian() {
+	if(document.getElementById("italian").innerHTML == "Italian (italien)"){
+		document.getElementById("italian").innerHTML = "";
+	} else {
 	document.getElementById("italian").innerHTML = "Italian (italien)";
+	}
 }
 function french() {
+	if(document.getElementById("french").innerHTML == "French (french)"){
+		document.getElementById("french").innerHTML = "";
+	} else {
 	document.getElementById("french").innerHTML = "French (french)";
+	}
 }
 function chinese() {
+	if(document.getElementById("chinese").innerHTML == "Chinese (chainis)"){
+		document.getElementById("chinese").innerHTML = "";
+	} else {
 	document.getElementById("chinese").innerHTML = "Chinese (chainis)";
+	}
 }
 function greek() {
+	if(document.getElementById("greek").innerHTML == "Greek (grik)"){
+		document.getElementById("greek").innerHTML = "";
+	} else {
 	document.getElementById("greek").innerHTML = "Greek (grik)";
+	}
 }
 function geo() {
-	document.getElementById("geo").innerHTML = "Geometry (chiometri)";
+	if(document.getElementById("geo").innerHTML == "Geometry (chiometry)"){
+		document.getElementById("geo").innerHTML = "";
+	} else {
+	document.getElementById("geo").innerHTML = "Geometry (chiometry)";
+	}
 }
 function ss() {
+	if(document.getElementById("ss").innerHTML == "Social Studies (soshial stodis)"){
+		document.getElementById("ss").innerHTML = "";
+	} else {
 	document.getElementById("ss").innerHTML = "Social Studies (soshial stodis)";
+	}
 }
 function lit() {
+	if(document.getElementById("lit").innerHTML == "English Literature (inglish literachure)"){
+		document.getElementById("lit").innerHTML = "";
+	} else {
 	document.getElementById("lit").innerHTML = "English Literature (inglish literachure)";
+	}
 }
 function sports() {
+	if(document.getElementById("sports").innerHTML == "Sports (sports)"){
+		document.getElementById("sports").innerHTML = "";
+	} else {
 	document.getElementById("sports").innerHTML = "Sports (sports)";
+	}
 }
 function latin() {
+	if(document.getElementById("latin").innerHTML == "Latin (latin)"){
+		document.getElementById("latin").innerHTML = "";
+	} else {
 	document.getElementById("latin").innerHTML = "Latin (latin)";
+	}
 }
 function calc() {
-	document.getElementById("calc").innerHTML = "Calculus (calciulos)";
+	if(document.getElementById("calc").innerHTML == "Calculus (calculos)"){
+		document.getElementById("calc").innerHTML = "";
+	} else {
+	document.getElementById("calc").innerHTML = "Calculus (calculos)";
+	}
 }
 function trig() {
+	if(document.getElementById("trig").innerHTML == "Trigonometry (trigonometri)"){
+		document.getElementById("trig").innerHTML = "";
+	} else {
 	document.getElementById("trig").innerHTML = "Trigonometry (trigonometri)";
+	}
 }
 function honors() {
+	if(document.getElementById("honors").innerHTML == "Honors Class (onors clas)"){
+		document.getElementById("honors").innerHTML = "";
+	} else {
 	document.getElementById("honors").innerHTML = "Honors Class (onors clas)";
+	}
 }
