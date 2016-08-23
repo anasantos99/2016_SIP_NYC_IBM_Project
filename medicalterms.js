a5bf0416ad6f50406660a6c2547628f0b3f2a8d3
jami22 <jamiles22@me.com>
2016-08-23 09:31:46 -0400

    update

--- medicalterms.js
+++ medicalterms.js
@@ -1,48 +1,112 @@
 function allergy() {
-	document.getElementById("allergy").innerHTML = "Allergy(alergi)";
+	if(document.getElementById("allergy").innerHTML == "Allergy(alergi)"){
+        document.getElementById("allergy").innerHTML = "";
+    } else {
+        document.getElementById("allergy").innerHTML = "Allergy(alergi)";
+    }
 }
 function alzheimersdisease() {
-	document.getElementById("alzheimersdisease").innerHTML = "Alzheimer's Disease(alzemers disease)";
+	if(document.getElementById("alzheimersdisease").innerHTML == "Alzheimer's Disease(alzemers disease)"){
+        document.getElementById("alzheimersdisease").innerHTML = "";
+    } else {
+        document.getElementById("alzheimersdisease").innerHTML = "Alzheimer's Disease(alzemers disease)";
+    }
 }
 function arthritis() {
-	document.getElementById("arthritis").innerHTML = "Arthitis(arthetes)";
-}
+	if(document.getElementById("arthritis").innerHTML == "Arthitis(arthetes)"){
+        document.getElementById("arthritis").innerHTML = "";
+    } else {
+        document.getElementById("arthritis").innerHTML = "Arthitis(arthetes)";
+    }
+} 
 function asthma() {
-	document.getElementById("asthma").innerHTML = "Asthma(asthma)";
+	if(document.getElementById("asthma").innerHTML == "Asthma(asthma)"){
+        document.getElementById("asthma").innerHTML = "";
+    } else {
+        document.getElementById("asthma").innerHTML = "Asthma(asthma)";
+    }
 }
 function cholesterol() {
-	document.getElementById("cholesterol").innerHTML = "Cholesterol(colestrol)";
+	if(document.getElementById("cholesterol").innerHTML == "Cholesterol(colestrol)"){
+        document.getElementById("cholesterol").innerHTML = "";
+    } else {
+        document.getElementById("cholesterol").innerHTML = "Cholesterol(colestrol)";
+    }
 }
 function chronicpain() {
-	document.getElementById("chronicpain").innerHTML = "Chronic Pain(cronic pain)";
+	if(document.getElementById("chronicpain").innerHTML == "Chronic Pain(cronic pain)"){
+        document.getElementById("chronicpain").innerHTML = "";
+    } else {
+        document.getElementById("chronicpain").innerHTML = "Chronic Pain(cronic pain)";
+    }
 }
 function flu() {
-	document.getElementById("flu").innerHTML = "Flu(flu)";
+	if(document.getElementById("flu").innerHTML == "Flu(flu)"){
+        document.getElementById("flu").innerHTML = "";
+    } else {
+        document.getElementById("flu").innerHTML = "Flu(flu)";
+    }
 }
 function depression() {
-	document.getElementById("depression").innerHTML = "Depression(depression)";
+	if(document.getElementById("depression").innerHTML == "Depression(depression)"){
+        document.getElementById("depression").innerHTML = "";
+    } else {
+        document.getElementById("depression").innerHTML = "Depression(depression)";
+    }
 }
 function abdominalpain() {
-	document.getElementById("abdominalpain").innerHTML = "Abdominal Pain(abdominal pain)";
+	if(document.getElementById("abdominalpain").innerHTML == "Abdominal Pain(abdominal pain)"){
+        document.getElementById("abdominalpain").innerHTML = "";
+    } else {
+        document.getElementById("abdominalpain").innerHTML = "Abdominal Pain(abdominal pain)";
+    }
 }
 function cataracts() {
-	document.getElementById("cataracts").innerHTML = "Cataracts(catarakts)";
+	if(document.getElementById("cataracts").innerHTML == "Cataracts(catarakts)"){
+        document.getElementById("cataracts").innerHTML = "";
+    } else {
+        document.getElementById("cataracts").innerHTML = "Cataracts(catarakts)";
+    }
 }
 function heartattack() {
-	document.getElementById("heartattack").innerHTML = "Heart Attack(hart attak)";
+	if(document.getElementById("heartattack").innerHTML == "Heart Attack(hart attak)"){
+        document.getElementById("heartattack").innerHTML = "";
+    } else {
+        document.getElementById("heartattack").innerHTML = "Heart Attack(hart attak)";
+    }
 }
 function highbloodpressure() {
-		document.getElementById("highbloodpressure").innerHTML = "High Blood Pressure(hi blod pressur)";
+		if(document.getElementById("highbloodpressure").innerHTML == "High Blood Pressure(hi blod pressur)"){
+            document.getElementById("highbloodpressure").innerHTML = "";
+        } else {
+            document.getElementById("highbloodpressure").innerHTML = "High Blood Pressure(hi blod pressur)";
+        }
 	}
 function menopause() {
-	document.getElementById("menopause").innerHTML = "Menopause(menopase)";
+	if(document.getElementById("menopause").innerHTML == "Menopause(menopase)"){
+        document.getElementById("menopause").innerHTML = "";
+    } else {
+        document.getElementById("menopause").innerHTML = "Menopause(menopase)";
+    }
 }
 function anxiety() {
-	document.getElementById("anxiety").innerHTML = "Anxiety(anxety)";
+	if(document.getElementById("anxiety").innerHTML == "Anxiety(anxety)"){
+        document.getElementById("anxiety").innerHTML = "";
+    } else {
+        document.getElementById("anxiety").innerHTML = "Anxiety(anxety)";
+    }
 }
 function migraine() {
-	document.getElementById("migraine").innerHTML = "Migraine(maigraine)";
+	if(document.getElementById("migraine").innerHTML == "Migraine(maigraine)"){
+        document.getElementById("migraine").innerHTML = "";
+    } else {
+        document.getElementById("migraine").innerHTML = "Migraine(maigraine)";
+    }
 }
 function insomnia() {
-	document.getElementById("insomnia").innerHTML = "Insomnia(insomnia)";
+	if(document.getElementById("insomnia").innerHTML == "Insomnia(insomnia)"){
+        document.getElementById("insomnia").innerHTML = "";
+    } else {
+        document.getElementById("insomnia").innerHTML = "Insomnia(insomnia)";
+    }
 }
