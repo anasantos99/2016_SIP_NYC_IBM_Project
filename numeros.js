2d31bd01a787056054a54a04e1e9d55ce928ee44
vanessamendoza <vanessam363@gmail.com>
2016-08-23 10:20:19 -0400

    Update numeros.js
    added if and else statements for numeros.js file

--- numeros.js
+++ numeros.js
@@ -1,33 +1,93 @@
 function one() {
-		document.getElementById("one").innerHTML = "One(wan)";
+    if(document.getElementById("one").innerHTML=="One(wan)"){
+		document.getElementById("one").innerHTML="";
+	}else{
+	document.getElementById("one").innerHTML = "One(wan)";
 	}
-function two() {
+}
+
+function two(){ 
+    if(document.getElementById("two").innerHTML=="Two(to)"){
+		document.getElementById("two").innerHTML="";
+	}else{
 	document.getElementById("two").innerHTML = "Two(to)";
+	}
 }
+
 function three() {
+    if(document.getElementById("three").innerHTML=="Three(three)"){
+		document.getElementById("three").innerHTML="";
+	}else{
 	document.getElementById("three").innerHTML = "Three(three)";
+	}
 }
+
 function four() {
+    if(document.getElementById("four").innerHTML=="Four(for)"){
+		document.getElementById("four").innerHTML="";
+	}else{
 	document.getElementById("four").innerHTML = "Four(for)";
+	}
 }
+
+
 function five() {
+     if(document.getElementById("five").innerHTML=="Five(five)"){
+		document.getElementById("five").innerHTML="";
+	}else{
 	document.getElementById("five").innerHTML = "Five(five)";
+	}
 }
+
 function six() {
+     if(document.getElementById("six").innerHTML=="Six(six)"){
+		document.getElementById("six").innerHTML="";
+	}else{
 	document.getElementById("six").innerHTML = "Six(six)";
+	}
 }
+
+
 function seven() {
+    if(document.getElementById("seven").innerHTML=="Seven(seven)"){
+		document.getElementById("seven").innerHTML="";
+	}else{
 	document.getElementById("seven").innerHTML = "Seven(seven)";
+	}
 }
+
 function eight() {
+    if(document.getElementById("eight").innerHTML=="Eight(ate)"){
+		document.getElementById("eight").innerHTML="";
+	}else{
 	document.getElementById("eight").innerHTML = "Eight(ate)";
+	}
 }
+
+	
 function nine() {
+    if(document.getElementById("nine").innerHTML=="Nine(nine)"){
+		document.getElementById("nine").innerHTML="";
+	}else{
 	document.getElementById("nine").innerHTML = "Nine(nine)";
+	}
 }
+
+
 function ten() {
+    if(document.getElementById("ten").innerHTML=="Ten(ten)"){
+		document.getElementById("ten").innerHTML="";
+	}else{
 	document.getElementById("ten").innerHTML = "Ten(ten)";
+	}
 }
+	
 function ninety() {
+      if(document.getElementById("ninety").innerHTML=="Ninety-Nine(nindy nine)"){
+		document.getElementById("ninety").innerHTML="";
+	}else{
 	document.getElementById("ninety").innerHTML = "Ninety-Nine(nindy nine)";
+	}
 }
+
+
