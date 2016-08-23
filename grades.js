7d3e1925d2199e6198ab8642dc261f51ccfc150c
anasantos99 <ana.santos.0818@gmail.com>
2016-08-23 10:38:04 -0400

    Update grades.js

--- grades.js
+++ grades.js
@@ -1,30 +1,70 @@
 function pass() {
+	if(document.getElementById("pass").innerHTML == "Pass the Class (pas de clas)"){
+		document.getElementById("pass").innerHTML = "";
+	} else {
 	document.getElementById("pass").innerHTML = "Pass the Class (pas de clas)";
+	}
 }
 function fail() {
-	document.getElementById("fail").innerHTML = "Fail the class (feil de clas)";
+	if(document.getElementById("fail").innerHTML == "Fail the Class (feil de clas)"){
+		document.getElementById("fail").innerHTML = "";
+	} else {
+	document.getElementById("fail").innerHTML = "Fail the Class (feil de clas)";
+	}
 }
 function excellent() {
+	if(document.getElementById("excellent").innerHTML == "Excellent (exselent)"){
+		document.getElementById("excellent").innerHTML = "";
+	} else {
 	document.getElementById("excellent").innerHTML = "Excellent (exselent)";
+	}
 }
 function great() {
+	if(document.getElementById("great").innerHTML == "Very Good (veri gud)"){
+		document.getElementById("great").innerHTML = "";
+	} else {
 	document.getElementById("great").innerHTML = "Very Good (veri gud)";
+	}
 }
 function poor() {
+	if(document.getElementById("poor").innerHTML == "Poor (por)"){
+		document.getElementById("poor").innerHTML = "";
+	} else {
 	document.getElementById("poor").innerHTML = "Poor (por)";
+	}
 }
 function average() {
+	if(document.getElementById("average").innerHTML == "Average (averech)"){
+		document.getElementById("average").innerHTML = "";
+	} else {
 	document.getElementById("average").innerHTML = "Average (averech)";
+	}
 }
 function improve() {
+	if(document.getElementById("improve").innerHTML == "Needs Improvement (nids impruvment)"){
+		document.getElementById("improve").innerHTML = "";
+	} else {
 	document.getElementById("improve").innerHTML = "Needs Improvement (nids impruvment)";
+	}
 }
 function progress() {
+	if(document.getElementById("progress").innerHTML == "Good Progress (gud progres)"){
+		document.getElementById("progress").innerHTML = "";
+	} else {
 	document.getElementById("progress").innerHTML = "Good Progress (gud progres)";
+	}
 }
 function danger() {
+	if(document.getElementById("danger").innerHTML == "In danger of failing the class (in dencher ov  feilin de clas)"){
+		document.getElementById("danger").innerHTML = "";
+	} else {
 	document.getElementById("danger").innerHTML = "In danger of failing the class (in dencher ov  feilin de clas)";
+	}
 }
 function advanced() {
+	if(document.getElementById("advanced").innerHTML == "Is ahead of the rest of the class (is ajed ov the rest ov de clas)"){
+		document.getElementById("advanced").innerHTML = "";
+	} else {
 	document.getElementById("advanced").innerHTML = "Is ahead of the rest of the class (is ajed ov the rest ov de clas)";
+	}
 }
